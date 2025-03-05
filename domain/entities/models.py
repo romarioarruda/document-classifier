ef79f5778535e81ab038966d32ce485e8973db62
Romário Arruda | Sossego <romario.arruda@sossego.com.br>
2025-03-05 13:56:43 -0300

    fix(models): update entity fields and validations

--- domain/entities/models.py
+++ domain/entities/models.py
@@ -1,16 +1,17 @@
-from uuid import UUID
-from typing import Dict
+from typing import List
 from pydantic import BaseModel, Field
+
+from domain.dtos.field_pattern import FieldPatternDTO
 
 
 class ModelEntity(BaseModel):
-    id: UUID
-    classification: str = Field(min_length=3, max_length=30)
-    classification_raw: str = Field(min_length=3, max_length=30)
-    text: str = Field(min_length=10, max_length=10000)
+    id: str = Field(alias='_id')
+    classification: str = Field(..., min_length=3, max_length=30)
+    classification_raw: str = Field(..., min_length=3, max_length=30, alias='classificationRaw')
+    text: str = Field(..., min_length=10, max_length=10000)
 
 
 class ModelFieldsEntity(BaseModel):
-    id: UUID
-    modelId: UUID
-    fields: Dict
+    id: str = Field(alias='_id')
+    model_id: str = Field(..., min_length=20, max_length=128, alias='modelId')
+    fields_and_patterns: List[FieldPatternDTO] = Field(..., alias='fieldsAndPatterns')
